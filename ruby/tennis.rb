e0b71969a66417c328f234b3e7ce6b4dfb5a2e3e
Yusuke Doi <yuusukedoi23@gmail.com>
2021-07-13 23:15:55 +0900

    wip

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -1,5 +1,11 @@
 
 class TennisGame1
+  ORDINAL_POINT = {
+    0 => "Love",
+    1 => "Fifteen",
+    2 => "Thirty",
+    3 => "Forty",
+  }
 
   def initialize(player1Name, player2Name)
     @player1Name = player1Name
@@ -7,52 +13,39 @@ class TennisGame1
     @p1points = 0
     @p2points = 0
   end
-        
+
   def won_point(playerName)
-    if playerName == "player1"
+    if playerName == @player1Name
       @p1points += 1
     else
       @p2points += 1
     end
   end
-  
+
   def score
-    result = ""
-    tempScore=0
-    if (@p1points==@p2points)
-      result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
+    if @p1points == @p2points
+      # 同点
+      {
+        0 => "Love-All",
+        1 => "Fifteen-All",
+        2 => "Thirty-All",
       }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage player1"
-      elsif (minusResult ==-1)
-        result ="Advantage player2"
-      elsif (minusResult>=2)
-        result = "Win for player1"
+    elsif @p1points >= 4 || @p2points >= 4
+      # 勝ちかアドバンテージ
+      point_diff = @p1points - @p2points
+
+      if point_diff == 1
+        "Advantage player1"
+      elsif point_diff == -1
+        "Advantage player2"
+      elsif point_diff >= 2
+        "Win for player1"
       else
-        result ="Win for player2"
+        "Win for player2"
       end
     else
-      (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
-        else
-          result+="-"
-          tempScore = @p2points
-        end
-        result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
-      end
+      "#{ORDINAL_POINT[@p1points]}-#{ORDINAL_POINT[@p2points]}"
     end
-    result
   end
 end
 
